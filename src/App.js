2f8564e315c70b2cdecd0038f8b5716e2905d87f
Jon Washington <jonathan.washington@gmail.com>
2021-02-21 17:01:32 -0800

    First major commit to github, includes ability to search Google books api for volumes and navigate to a volumes page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory
+} from "react-router-dom";
 
-function App() {
+import HomePage from './components/HomePage';
+import SearchBar from './components/SearchBar';
+import Results from './components/Results';
+import BookDetail from './components/BookDetail';
+
+function App(props) {
+
+  const [searchValue, setSearch] = useState('search for something');
+  const [results, updateResults] = useState(null);
+  const [activeBook, setActiveBook] = useState(null);
+  // const [] = useState();
+
+  let history = useHistory();
+
+  useEffect(() => {
+    if (activeBook && activeBook.id){
+      history.push('/volume/' + activeBook.id);
+    }
+  }, [activeBook]);
+
+  const searchForBook = searchValue => {
+    setSearch(searchValue);
+    let searchString = 'https://www.googleapis.com/books/v1/volumes?q='
+    + searchValue.searchValue + '&key=' +
+    process.env.REACT_APP_GOOGLE_BOOKS_KEY;
+
+    fetch(searchString)
+      .then(res => res.json())
+        .then(
+          (result) =>
+          {
+            let resultItems = result.items.map(obj => {
+              let newObj = {};
+              // reduce our search results to the volume info
+              newObj = obj.volumeInfo;
+              // make sure the gBooks-specific id carries over
+              newObj.id = obj.id;
+              return newObj;
+            });
+            updateResults(resultItems);
+            console.log({results});
+          }
+        )
+  }
+
+  const displayBookPage = book => {
+    console.log(book);
+    setActiveBook(book);
+    console.log(activeBook);
+    // history.push('/volume/' + id);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <p id='title'>BookBug</p>
+        <SearchBar searchForBook={searchForBook}/>
       </header>
+        <div className='body'>
+          <Route path='/'
+            render={(props) => (
+              <HomePage />
+            )}
+          />
+          <Results
+            results={results}
+            displayBookPage={displayBookPage}
+          />
+          <Route path='/volume/:id'
+            render={(props) => (
+              <BookDetail activeBook={activeBook} />
+            )}
+          />
+        </div>
     </div>
   );
 }
